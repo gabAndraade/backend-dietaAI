71472b46fcab7c273875abd04ac51f32af80a9de
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-09-24 17:08:39 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,31 @@ import { routes } from './routes';
 const app = fastify({ logger: true });
 dotenv.config();
 
+// Tratamento de erros
 app.setErrorHandler((error, request, reply) => {
     reply.code(400).send({ message: error.message });
 });
 
-// Configuração do CORS para permitir a origem do frontend
+// Configuração de CORS
 app.register(cors, {
-  origin: process.env.ALLOWED_ORIGIN || 'https://wondrous-semifreddo-cfdac0.netlify.app',
+    origin: process.env.ALLOWED_ORIGIN || 'https://wondrous-semifreddo-cfdac0.netlify.app',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
 });
 
+// ** Adicione o handler para as requisições preflight (OPTIONS) aqui **
+app.options('*', (request, reply) => {
+  reply
+    .header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    .header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
+    .header('Access-Control-Allow-Headers', 'Content-Type')
+    .send();
+});
+
+// Registro das rotas
 app.register(routes);
 
+// Inicialização do servidor
 const start = async () => {
     try {
         await app.listen({ port: 3333, host: '0.0.0.0' });
