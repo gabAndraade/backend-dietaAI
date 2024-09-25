f94071d2de95dbde9ac219a5bcacdd872e937a99
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-09-25 17:28:08 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,27 @@
-import fastify from 'fastify';
-import cors from '@fastify/cors';
-import dotenv from 'dotenv';
-import { routes } from './routes';
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import dotenv from 'dotenv'
+import { routes } from './routes'
 
-const app = fastify({ logger: true });
+const app = Fastify({ logger: true })
 dotenv.config();
 
-// Tratamento de erros
 app.setErrorHandler((error, request, reply) => {
-    reply.code(400).send({ message: error.message });
-});
+  reply.code(400).send({ message: error.message })
+})
 
-// Configuração de CORS
-app.register(require('@fastify/cors'), {
-  origin: process.env.ALLOWED_ORIGIN || 'https://main--wondrous-semifreddo-cfdac0.netlify.app/nutrition',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type'],
-});
-
-// ** Adicione o handler para as requisições preflight (OPTIONS) aqui **
-app.options('*', (request, reply) => {
-  reply
-    .header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    .header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
-    .header('Access-Control-Allow-Headers', 'Content-Type')
-    .send();
-});
-
-// Registro das rotas
-app.register(routes);
-
-// Inicialização do servidor
 const start = async () => {
-    try {
-        await app.listen({ port: 3333, host: '0.0.0.0' });
-        console.log(`Servidor rodando no http://localhost:3333`);
-    } catch (err) {
-        console.log(err);
-    }
-};
+  app.register(cors);
+  app.register(routes)
+
+
+  try{
+    await app.listen({ port: 3333, host: "0.0.0.0"})
+    console.log(`Servidor rodando no http://localhost:3333`)
+  }catch(err){
+    console.log(err);
+  }
+
+}
 
 start();
