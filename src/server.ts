070afff2a1302c0ba6f593f0cb540153be49d56b
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2024-09-26 20:10:36 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,38 @@
-import Fastify from "fastify";
-import cors from '@fastify/cors'
-import dotenv from 'dotenv'
-import { routes } from './routes'
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import dotenv from 'dotenv';
+import { routes } from './routes';
 
-const app = Fastify({ logger: true })
+const app = fastify({ logger: true });
 dotenv.config();
 
 app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({ message: error.message })
-})
+    reply.code(400).send({ message: error.message });
+});
+
+app.register(require('@fastify/cors'), {
+  origin: process.env.ALLOWED_ORIGIN || '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+});
+
+app.options('*', (request, reply) => {
+  reply
+    .header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    .header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
+    .header('Access-Control-Allow-Headers', 'Content-Type')
+    .send();
+});
+
+app.register(routes);
 
 const start = async () => {
-  app.register(cors);
-  app.register(routes)
-
-
-  try{
-    await app.listen({ port: 3333, host: "0.0.0.0"})
-    console.log(`Servidor rodando no http://localhost:3333`)
-  }catch(err){
-    console.log(err);
-  }
-
-}
+    try {
+        await app.listen({ port: 3333, host: '0.0.0.0' });
+        console.log(Servidor rodando no http://localhost:3333);
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 start();
