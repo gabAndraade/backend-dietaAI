ad78be382adf20f8a2e2e54f9ca2a080711a0d5b
Gabriel Andrade <160685894+gabAndraade@users.noreply.github.com>
2026-03-17 13:49:41 -0300

    Enhance server with CORS and serverless handler
    Added CORS configuration for specific origins and methods, and implemented a serverless handler for Vercel.

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,46 @@
+// server.ts
 import Fastify from "fastify";
-import cors from '@fastify/cors'
-import dotenv from 'dotenv'
-import { routes } from './routes'
+import cors from "@fastify/cors";
+import dotenv from "dotenv";
+import { routes } from "./routes";
 
-const app = Fastify({ logger: true })
 dotenv.config();
 
-app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({ message: error.message })
-})
+const app = Fastify({ logger: true });
 
-const start = async () => {
-  app.register(cors);
-  app.register(routes)
+app.register(cors, {
+  origin: "https://wondrous-semifreddo-cfdac0.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+  exposedHeaders: ["Content-Type"],
+  credentials: true,
+  strictPreflight: false, // <-- importante para Vercel
+  preflight: true,
+});
 
+app.register(routes);
 
-  try{
-    await app.listen({ port: 3333, host: "0.0.0.0"})
-    console.log(`Servidor rodando no http://localhost:3333`)
-  }catch(err){
-    console.log(err);
+// Handler para Vercel (Serverless)
+let isReady = false;
+
+export default async function handler(req: any, res: any) {
+  if (!isReady) {
+    await app.ready();
+    isReady = true; // evita chamar ready() em toda requisição
   }
-
+  app.server.emit("request", req, res);
 }
 
-start();
+// Start local
+if (process.env.NODE_ENV !== "production") {
+  const start = async () => {
+    try {
+      await app.listen({ port: 3333, host: "0.0.0.0" });
+      console.log("Servidor rodando no http://localhost:3333");
+    } catch (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+  };
+  start();
+}
